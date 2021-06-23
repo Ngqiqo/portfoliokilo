ee2aae60246bcab9c78cba15f66b4d06a1b31902
Ngqiqo <ngqiqoy@gmail.com>
2021-06-23 20:09:40 +0200

    Commit

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -14,14 +14,13 @@ from pathlib import Path
 import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9nrn=&2hp_6#ux(i(yi%4)v)yv75#ve8ei7+$@7ipl%gpt(ok&'
+SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -120,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
 django_heroku.settings(locals())
--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path, include
 
-from django.urls import path, include 
 from django.contrib import admin
-from django.urls import path
+
+admin.autodiscover()
+
 
 urlpatterns = [
     path('port/', include('port.urls')),
